90d38b628cd8344a03272524633874b2e1cdc10a
abwelch <abw28@zips.uakron.edu>
2020-07-16 20:05:47 -0500

    Add comment for intention

--- DataStructureTesting/Models/TestingHub.cs
+++ DataStructureTesting/Models/TestingHub.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace DataStructureTesting.Models
 {
+    // Will be used to generate all the elements and test against any of the data structures implemented
     public class TestingHub
     {
     }
