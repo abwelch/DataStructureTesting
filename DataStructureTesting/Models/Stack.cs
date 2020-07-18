cef3f44ca1c646c794b61afe07fe7952c8b4b4e3
abwelch <abw28@zips.uakron.edu>
2020-07-17 20:52:45 -0500

    Add comments and remove unused using directives

--- DataStructureTesting/Models/Stack.cs
+++ DataStructureTesting/Models/Stack.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
-
-namespace DataStructureTesting.Models
+﻿namespace DataStructureTesting.Models
 {
+    // A standard Stack data structure implemented with a singly linked list
     public class Stack
     {
         public SinglyLinkedList Zelda { get; }
@@ -14,18 +8,20 @@ namespace DataStructureTesting.Models
         public Stack()
         {
             Zelda = new SinglyLinkedList();
-
         }
+
         public Stack(int val)
         {
             Zelda = new SinglyLinkedList(val);
         }
 
+        // Time Complexity: O(n)
         public bool Push(int val)
         {
             return Zelda.Insert(val, 0);
         }
 
+        // Time Complexity: O(1)
         public int? Pop()
         {
             int? x = Zelda.RetrieveAtPosition(0);
@@ -34,6 +30,7 @@ namespace DataStructureTesting.Models
             return x;
         }
 
+        // Time Complexity: O(1)
         public int? Peek()
         {
             return Zelda.RetrieveAtPosition(0);
@@ -41,33 +38,35 @@ namespace DataStructureTesting.Models
 
 
         // returns number of elements in the stack
+        // Time Complexity: O(n)
         public int Size()
         {
             return Zelda.NodeCount;
         }
+
         // returns bool if the stack is empty or not
+        // Time Complexity: O(1)
         public bool IsEmpty()
         {
-            if(Zelda.NodeCount == 0)
+            if (Zelda.NodeCount == 0)
             {
                 return true;
             }
             return false;
         }
 
-
-        //scans the stack for a certain value, returns true or false 
+        // scans the stack for a certain value, returns true or false 
+        // Time Complexity: O(n)
         public bool Contains(int val)
         {
             return Zelda.Contains(val);
 
         }
         // returns value in a certain position
+        // Time Complexity: O(n)
         public int? ValueAtPosition(int pos)
         {
             return Zelda.RetrieveAtPosition(pos);
         }
     }
-
-
 }
