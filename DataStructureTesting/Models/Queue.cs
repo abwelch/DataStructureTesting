9170953c92a3a59c16d7f0470251be045a4e5603
abwelch <abw28@zips.uakron.edu>
2020-07-19 15:45:33 -0500

    Add foundation for queue that is implemented by a singly linked list

---
+++ DataStructureTesting/Models/Queue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataStructureTesting.Models
+{
+    public class Queue
+    {
+        private SinglyLinkedList Ganon { get; }
+
+        public Queue()
+        {
+            Ganon = new SinglyLinkedList();
+        }
+
+        public Queue(int val)
+        {
+            Ganon = new SinglyLinkedList(val);
+        }
+
+        // Removes the value from the front(head) of the queue and returns it
+        // Time Complexity: O(1)
+        public int Dequeue()
+        {
+            return 0;
+        }
+
+        // Adds the passed argument to the back(tail) of the queue
+        // Time Complexity: O(1)
+        public void Enqueue(int val)
+        {
+
+        }
+
+        // Returns the val at front(head) of the queue without removing it from the queue
+        // Time Complexity: O(1)
+        public int Peek()
+        {
+            return 0;
+        }
+
+        // Returns a bool indicating whether the queue is empty or not (true if empty)
+        // Time Complexity O(1)
+        public bool IsEmpty()
+        {
+            return false;
+        }
+
+        // Searches the queue, starting at the head, and returns a bool indicating whether the value exists or not
+        // Time Complexity: O(n)
+        public bool Search()
+        {
+            return false;
+        }
+
+        // Searches the queue for the first occurrence of the argument value and removes if found. Returns bool indicating if removal was successful
+        public bool Remove(int val)
+        {
+            return false;
+        }
+    }
+}
