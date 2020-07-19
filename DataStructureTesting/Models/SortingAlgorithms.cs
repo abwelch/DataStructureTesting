db72eb22bb4ce131f3099f284fc5cc8f21625eb6
abwelch <abw28@zips.uakron.edu>
2020-07-19 17:00:50 -0500

    Add foundation for various sorting algorithms that will work with an int array

---
+++ DataStructureTesting/Models/SortingAlgorithms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RunTimeAnalyzer.Models
+{
+    public class SortingAlgorithms
+    {
+        public void BubbleSort(int[] array)
+        {
+
+        }
+
+        public void SelectionSort(int[] array)
+        {
+
+        }
+
+        public void MergeSort(int[] array)
+        {
+
+        }
+
+        public void QuickSort(int[] array)
+        {
+
+        }
+    }
+}
