28170d1469b0aeb067234835fbd12d68453d9b28
abwelch <abw28@zips.uakron.edu>
2020-07-18 22:38:59 -0500

    Implement beginning of flashcards class

--- DataStructureTesting/ViewModels/FlashCards.cs
+++ DataStructureTesting/ViewModels/FlashCards.cs
@@ -14,13 +14,13 @@ namespace RunTimeAnalyzer.ViewModels
             Cards = new List<Tuple<string, string>>
             {
                 #region [Singly Linked List]
-                new Tuple<string, string>("Singly Linked List: Search", "Time Complexity: O(n)\nExplanation: Starting at the head node, there is potential for the entire list to be traversed."),
-                new Tuple<string, string>("Singly Linked List: Append", "Time Complexity: O(1)\nExplanation: Because a Tail property was implemented and maintained, this operation can occur in consant time. However, if a Tail is not implemented, this operation is O(n) time."),
-                new Tuple<string, string>("Singly Linked List: Insert", "Time Complexity: O(n)\nExplanation: Even with Tail, going to second to last node in singly list can lead to n time with n is exceptionally large."),
-                new Tuple<string, string>("Singly Linked List: Remove", "Time Complexity: O(n)\nExplanation: Even with Tail, going to second to last node in singly list can lead to n time with n is exceptionally large."),
-                new Tuple<string, string>("Singly Linked List: Insert at Head", "Time Complexity: O(1)\nExplanation: Because we have a reference to Head, this operation is constant."),
-                new Tuple<string, string>("Singly Linked List: Remove at Head", "Time Complexity: O(1)\nExplanation: Because we have a reference to Head, this operation is constant."),
-                new Tuple<string, string>("Singly Linked List: Reverse", "Time Complexity: O(n)\nExplanation: This requires traversing the entire list to change the references at each node.")
+                new Tuple<string, string>("Singly Linked List: Search", "Time Complexity: O(n). Explanation: Starting at the head node, there is potential for the entire list to be traversed."),
+                new Tuple<string, string>("Singly Linked List: Append", "Time Complexity: O(1). Explanation: Because a Tail property was implemented and maintained, this operation can occur in consant time. However, if a Tail is not implemented, this operation is O(n) time."),
+                new Tuple<string, string>("Singly Linked List: Insert", "Time Complexity: O(n). Explanation: Even with Tail, going to second to last node in singly list can lead to n time with n is exceptionally large."),
+                new Tuple<string, string>("Singly Linked List: Remove", "Time Complexity: O(n). Explanation: Even with Tail, going to second to last node in singly list can lead to n time with n is exceptionally large."),
+                new Tuple<string, string>("Singly Linked List: Insert at Head", "Time Complexity: O(1). Explanation: Because we have a reference to Head, this operation is constant."),
+                new Tuple<string, string>("Singly Linked List: Remove at Head", "Time Complexity: O(1). Explanation: Because we have a reference to Head, this operation is constant."),
+                new Tuple<string, string>("Singly Linked List: Reverse", "Time Complexity: O(n). Explanation: This requires traversing the entire list to change the references at each node.")
                 #endregion
             };
         }
