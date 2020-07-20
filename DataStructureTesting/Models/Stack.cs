d4547c78e5312493fb0c5f49d68f62663e6845a5
abwelch <abw28@zips.uakron.edu>
2020-07-20 16:33:24 -0500

    Expand comments

--- DataStructureTesting/Models/Stack.cs
+++ DataStructureTesting/Models/Stack.cs
@@ -15,21 +15,23 @@
             Zelda = new SinglyLinkedList(val);
         }
 
-        // Time Complexity: O(n)
+        // Adds the new value to the top of the stack
+        // Time Complexity: O(1)
         public bool Push(int val)
         {
             return Zelda.Insert(val, 0);
         }
 
+        // Removes value from the top of the stack and returns
         // Time Complexity: O(1)
         public int? Pop()
         {
             int? x = Zelda.RetrieveAtPosition(0);
             Zelda.RemoveAt(x);
-
             return x;
         }
 
+        // View value at top of the stack
         // Time Complexity: O(1)
         public int? Peek()
         {
@@ -37,14 +39,14 @@
         }
 
 
-        // returns number of elements in the stack
-        // Time Complexity: O(n)
+        // Returns number of elements in the stack
+        // Time Complexity: O(1)
         public int Size()
         {
             return Zelda.NodeCount;
         }
 
-        // returns bool if the stack is empty or not
+        // Returns bool if the stack is empty or not
         // Time Complexity: O(1)
         public bool IsEmpty()
         {
@@ -55,21 +57,21 @@
             return false;
         }
 
-        // scans the stack for a certain value, returns true or false 
+        // Scans the stack for a certain value, returns true or false 
         // Time Complexity: O(n)
         public bool Contains(int val)
         {
             return Zelda.Contains(val);
 
         }
-        // returns value in a certain position
+        // Returns value in a certain position
         // Time Complexity: O(n)
         public int? ValueAtPosition(int pos)
         {
             return Zelda.RetrieveAtPosition(pos);
         }
 
-
+        // Prints elements in the stack, starting at Head
         // Time Complexity: O(n)
         public void PrintStackContent()
         {
