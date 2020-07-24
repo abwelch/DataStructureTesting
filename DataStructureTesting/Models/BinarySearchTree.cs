da791c995dca637b5513fcfb728f3cb6fc24bc1a
abwelch <abw28@zips.uakron.edu>
2020-07-24 17:10:22 -0500

    Create foundation for BST data structure (derived from binary tree)

---
+++ DataStructureTesting/Models/BinarySearchTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RunTimeAnalyzer.Models
+{
+    // BST is a binary tree that satisfies the BST invariant: left subtree has smaller elements than the right subtree
+    public class BinarySearchTree : BinaryTree
+    {
+        public BinarySearchTree() : base()
+        { }
+
+        public BinarySearchTree(int val) : base(val)
+        { }
+
+    }
+}
