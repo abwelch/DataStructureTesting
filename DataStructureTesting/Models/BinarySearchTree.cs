98f7a6f830194e77c7f2f4bf1cf6a32974f6dd8e
abwelch <abw28@zips.uakron.edu>
2020-07-24 22:01:35 -0500

    Begin implementing two versions of insert

--- DataStructureTesting/Models/BinarySearchTree.cs
+++ DataStructureTesting/Models/BinarySearchTree.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 namespace RunTimeAnalyzer.Models
 {
     // BST is a binary tree that satisfies the BST invariant: left subtree has smaller elements than the right subtree
+    // This implementation allows for duplicate values
+    // Time Complexity: Average: O(log n). Worst: O(n) if tree degenerates to linked list (all elements increasing/decreasing)
     public class BinarySearchTree : BinaryTree
     {
         public BinarySearchTree() : base()
@@ -14,5 +16,50 @@ namespace RunTimeAnalyzer.Models
         public BinarySearchTree(int val) : base(val)
         { }
 
+        public void InsertIterative(int val)
+        {
+            Node ptr = Root;
+            while (true)
+            {
+                if (val > ptr.Value)
+                {
+                    if (ptr.RightChild == null)
+                        ptr.RightChild = new Node(val);
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+
+        // Publicly accessible method to call InsertRecursive bc Root and Node are not publicly exposed
+        public void CallInsertRecursive(int val)
+            => InsertRecursive(val, Root);
+
+        protected void InsertRecursive(int val, Node ptr)
+        {
+            // Base case
+            if (ptr == null)
+            {
+                ptr = new Node(val);
+                return;
+            }
+
+            if (val > ptr.Value)
+            {
+                //ptr = 
+            }
+            else
+            {
+
+            }
+        }
+
+        public bool Search(int val)
+        {
+            return true;
+        }
     }
 }
