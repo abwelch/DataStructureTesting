434e21276a5d361ae6e47b3bc7fc9cec2cfe57cf
abwelch <abw28@zips.uakron.edu>
2020-07-25 15:07:30 -0500

    Complete recursive and iterative insert methods. Implement binary search method

--- DataStructureTesting/Models/BinarySearchTree.cs
+++ DataStructureTesting/Models/BinarySearchTree.cs
@@ -16,35 +16,57 @@ namespace RunTimeAnalyzer.Models
         public BinarySearchTree(int val) : base(val)
         { }
 
+        // Iterative insert
+        // Time Complexity: Average O(log n). Worst O(n)
+        // Worst occurs if tree has degenerated into a list due to ever increasing or decreasing values inserted
         public void InsertIterative(int val)
         {
             Node ptr = Root;
+            // Traverse until a leaf is reached
             while (true)
             {
+                // Disallow duplicates
+                if (val == ptr.Value)
+                    return;
                 if (val > ptr.Value)
                 {
                     if (ptr.RightChild == null)
+                    {
                         ptr.RightChild = new Node(val);
+                        TotalNodes++;
+                        return;
+                    }
+                    else
+                    {
+                        ptr = ptr.RightChild;
+                    }
                 }
                 else
                 {
-
+                    if (ptr.LeftChild == null)
+                    {
+                        ptr.LeftChild = new Node(val);
+                        TotalNodes++;
+                        return;
+                    }
+                    else
+                    {
+                        ptr = ptr.LeftChild;
+                    }
                 }
             }
-
         }
 
-        // Publicly accessible method to call InsertRecursive bc Root and Node are not publicly exposed
-        public void CallInsertRecursive(int val)
-            => InsertRecursive(val, Root);
-
         // Recursively insert
+        // Time Complexity: Average O(log n). Worst O(n)
+        // Worst occurs if tree has degenerated into a list due to ever increasing or decreasing values inserted
         protected Node InsertRecursive(int val, Node ptr)
         {
             // Base case
             if (ptr == null)
             {
                 ptr = new Node(val);
+                TotalNodes++;
             }
             else if (val > ptr.Value)
             {
@@ -57,25 +79,31 @@ namespace RunTimeAnalyzer.Models
             return ptr;
         }
 
-        public void PrintBreadthFirst()
-        {
-            Queue<Node> q = new Queue<Node>();
-            q.Enqueue(Root);
-            while (q.Count > 0)
-            {
-                Node current = q.Dequeue();
-                if (current == null)
-                    continue;
-                q.Enqueue(current.LeftChild);
-                q.Enqueue(current.RightChild);
-                
-                // print
-            }            
-        }
+        // Publicly accessible method to call InsertRecursive bc Root and Node are not publicly exposed
+        public void CallInsertRecursive(int val)
+            => InsertRecursive(val, Root);
 
+        // Conduct a binary search for specified value
+        // Time Complexity: Average O(log n). Worst O(n) if tree degrades into a list
         public bool Search(int val)
         {
-            return true;
+            Node ptr = Root;
+            while (ptr != null)
+            {
+                if (val == ptr.Value)
+                {
+                    return true;
+                }
+                else if (val > ptr.Value)
+                {
+                    ptr = ptr.RightChild;
+                }
+                else
+                {
+                    ptr = ptr.LeftChild;
+                }
+            }
+            return false;
         }
     }
 }
