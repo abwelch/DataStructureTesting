c24f0a8be34b6a1eb85125b50510b9145aa4c25f
abwelch <abw28@zips.uakron.edu>
2020-07-26 21:26:35 -0500

    Create static class with list of options

---
+++ DataStructureTesting/ViewModels/InteractiveOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace RunTimeAnalyzer.ViewModels
+{
+    public static class InteractiveOptions
+    {
+        public static List<string> Options { get; }
+        static InteractiveOptions()
+        {
+            Options = new List<string>
+            {
+                "Singly Linked List",
+                "Doubly Linked List",
+                "Stack",
+                "Queue",
+                "Circular Queue",
+                "Binary Tree",
+                "Binary Search Tree"
+            };
+        }
+    }
+}
